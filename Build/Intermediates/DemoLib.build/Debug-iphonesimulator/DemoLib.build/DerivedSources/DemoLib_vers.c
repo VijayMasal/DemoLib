e868e990dc4584ab5f088c2e00ac2187aaa680b5
vijay <vdmasal@gmail.com>
2017-12-15 15:17:11 +0530

    Initial Commit

---
+++ Build/Intermediates/DemoLib.build/Debug-iphonesimulator/DemoLib.build/DerivedSources/DemoLib_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char DemoLibVersionString[];
+ extern const double DemoLibVersionNumber;
+
+ const unsigned char DemoLibVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:DemoLib  PROJECT:DemoLib-1" "\n";
+ const double DemoLibVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ DemoLib/DemoLib.h
@@ -0,0 +1,19 @@
+//
+//  DemoLib.h
+//  DemoLib
+//
+//  Created by Emberify_Vijay on 15/12/17.
+//  Copyright © 2017 Vijay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoLib.
+FOUNDATION_EXPORT double DemoLibVersionNumber;
+
+//! Project version string for DemoLib.
+FOUNDATION_EXPORT const unsigned char DemoLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoLib/PublicHeader.h>
+
+
